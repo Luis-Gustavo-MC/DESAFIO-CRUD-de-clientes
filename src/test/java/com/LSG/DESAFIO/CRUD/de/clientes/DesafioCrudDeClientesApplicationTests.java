a37515c38816383c98841337cce54a353ddd016c
Luis Gustavo <lgmc120@gmail.com>
2025-03-15 02:05:34 -0300

    CREATE

---
+++ src/test/java/com/LSG/DESAFIO/CRUD/de/clientes/DesafioCrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LSG.DESAFIO.CRUD.de.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
